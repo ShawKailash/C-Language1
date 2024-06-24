b5da03372438dffcd84c08c38700d46c1b95dc4f
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-24 13:57:31 +0530

    Add files via upload

---
+++ C Language 1/9_Format_Specifier.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+    int a=5;
+    float b=6.7896545;
+    float c=4.87687678;
+    printf("the value of a is %d and the value of b is %f and the value of c is %0.2f\n",a,b,c);
+    const float d=7.6678;
+    printf("%f\n",PI);
+    printf("tab character \t backslash \\ window song \a new line \n complete");
+    return 0;
+}
