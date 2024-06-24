cd45a376a959a10aee9e667c0ac8a6e093e800b1
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-24 13:56:11 +0530

    Add files via upload

---
+++ C Language 1/7_Operator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    a=40;
+    b=6;
+    c=2;
+    d=0;
+    printf("a + b = %d\n",a+b);
+    printf("a - b = %d\n",a-b);
+    printf("a * b = %d\n",a*b);
+    printf("a / b = %d\n",a/b);
+    printf("c == d = %d\n",c==d);
+    printf("c && d = %d\n",c&&d);
+    printf("c || d = %d\n",c||d);
+    printf("! c = %d\n",!c);
+    printf("! d = %d\n",!d);
+    return 0;
+}
