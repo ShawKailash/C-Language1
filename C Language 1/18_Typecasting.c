02e21e3e5b7ea56a6e76bcdce0f23bbc4f4eebf9
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-26 17:09:53 +0530

    Add files via upload

---
+++ C Language 1/18_Typecasting.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=3;
+    float b=54;
+    float c=(float)54/4;
+    printf("the value of a is %d\n",a);
+    printf("the value of b is %d\n",(int)b);
+    printf("the value of b is %f\n",b);
+    printf("the value of c is %f\n",c);
+    return 0;
+}
