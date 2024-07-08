3e2e00004682902d4d1f92524f45a83c8a69592e
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-07-08 20:49:14 +0530

    Add files via upload

---
+++ C Language 1/75_Memory_Leak.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a, i=0;
+    int *i2;
+    while(i < 78797)
+    {
+        printf("welcome to shaw kailash\n");
+        i2 = malloc(45443 * sizeof(int));
+        if(i % 100 == 0)
+        {
+            getchar();
+        }
+        i++;
+        free(i2);
+    }
+    return 0;
+}
